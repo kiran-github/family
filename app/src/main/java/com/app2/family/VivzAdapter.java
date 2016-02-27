8fb94fb7e3c18fe82046f088519592bd8287e103
kiran-github <kiranbala.subudhi@gmail.com>
2016-02-27 21:28:42 +0530

    Session 2 Assignment -by kiranbala

--- app/src/main/java/com/app2/family/VivzAdapter.java
+++ app/src/main/java/com/app2/family/VivzAdapter.java
@@ -8,15 +8,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by Kiran on 13-02-2016.
- */
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+
 public class VivzAdapter extends ArrayAdapter<String>
 {
     Context context;
     int[] images;
     String[] memberArray;
     String[] ageArray;
+     Firebase firebaseref;
     VivzAdapter(Context c,String[] members,int imgs[], String[] ages)
     {
         super(c,R.layout.single_row,R.id.textView,members);
@@ -24,18 +29,39 @@ public class VivzAdapter extends ArrayAdapter<String>
         this.images=imgs;
         this.memberArray=members;
         this.ageArray=ages;
+        Firebase.setAndroidContext(getContext());
+        firebaseref = new Firebase("https://assignmentfamily.firebaseio.com") ;
     }
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row=inflater.inflate(R.layout.single_row, parent, false)  ;
-        ImageView myImage=(ImageView) row.findViewById(R.id.imageView);
-        TextView myMember=(TextView)  row.findViewById(R.id.textView)  ;
-        TextView myAge=(TextView) row.findViewById(R.id.textView2);
+        final ImageView myImage=(ImageView) row.findViewById(R.id.imageView);
+        final TextView myMember=(TextView)  row.findViewById(R.id.textView)  ;
+        final TextView myAge=(TextView) row.findViewById(R.id.textView2);
 
         myImage.setImageResource(images[position]);
         myMember.setText(memberArray[position]);
         myAge.setText(ageArray[position]);
+
+
+        firebaseref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                 myMember.setText(String.valueOf(dataSnapshot.child("relation").child(String.valueOf(position)).child("Name").getValue()));
+                myAge.setText(String.valueOf(dataSnapshot.child("relation").child(String.valueOf(position)).child("relation").getValue()));
+                Picasso.with(getContext()).load(String.valueOf(dataSnapshot.child("relation").child(String.valueOf(position)).child("image").getValue())).into(myImage);
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
 
         return row;
     }
